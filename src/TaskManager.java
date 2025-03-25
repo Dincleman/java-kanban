7915fd534d576aa94f50700a6b2d1eca731c5875
Lisa <bobronhaim@yandex.ru>
2025-03-25 10:30:33 +0500

    Выполнение ФЗ5(исправления ч2)

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,15 +1,35 @@
 import java.util.List;
 
 public interface TaskManager {
+
+    // Задачи
     int addNewTask(Task task);
     Task getTask(int id);
     List<Task> getTasks();
-    List<Subtask> getSubtasks();
-    List<Epic> getEpics();
     void removeTask(int id);
-    void removeSubtask(int id);
-    void removeEpic(int id);
-    List<Subtask> getEpicSubtasks(int epicId);
-    List<Task> getHistory();
-}
 
+    //Подзадачи
+    int addNewSubtask(Subtask subtask);
+    Subtask getSubtask(int id);
+    List<Subtask> getSubtasks();
+    void removeSubtask(int id);
+
+    //Эпики
+    int addNewEpic(Epic epic);
+    Epic getEpic(int id);
+    List<Epic> getEpics();
+    void removeEpic(int id);
+
+    //Обновление задач, подзадач и эпиков
+    void updateTask(Task task);
+    void updateSubtask(Subtask subtask);
+    void updateEpic(Epic epic);
+
+    //История
+    List<Task> getHistory();
+
+    //Удаление всех задач, подзадач и эпиков
+    void removeAllTasks();
+    void removeAllSubtasks();
+    void removeAllEpics();
+}
