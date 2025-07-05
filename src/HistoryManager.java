ffcab047716a86c5acaffffd3f10f02c4d442538
Lisa <bobronhaim@yandex.ru>
2025-07-05 18:58:24 +0500

    Выполнение ФЗ6

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -1,7 +1,10 @@
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task); // Добавить задачу в историю
+
     void remove(int id); // Удалить задачу из истории по id
+
     List<Task> getHistory(); // Получить историю задач
 }
