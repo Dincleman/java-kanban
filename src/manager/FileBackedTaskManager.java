87e3e0634e09222a4f38d7378e893b746896942a
Lisa <bobronhaim@yandex.ru>
2025-07-24 10:55:09 +0500

    Выполнение ФЗ7: 1. создан класс FileBackedTaskManager

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -1,9 +1,13 @@
 package manager;
 
+import tasks.Task;
+
 import java.io.File;
 
 public class FileBackedTaskManager extends InMemoryTaskManager { // наследование с возможностью сохранения данных в файл
     private final File file;
+    private void save() { //создадим метод, кот. будет сохранять все задачи, подзадачи и эпики.
+    }
 
     public FileBackedTaskManager(File file) {
         this.file = file;
@@ -11,5 +15,14 @@ public class FileBackedTaskManager extends InMemoryTaskManager { // наслед
 
     //переопределим методы
     @Override
+    public int addNewTask(Task task) {
+        int id = super.addNewTask(task);
+        save();
+        return id;
+    }
+
+
+
+
 
 
