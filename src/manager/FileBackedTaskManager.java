6eff5259623f94a73a30b16cf313db3f10fae15a
Lisa <bobronhaim@yandex.ru>
2025-07-24 10:45:30 +0500

    Выполнение ФЗ6

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -1,5 +1,15 @@
 package manager;
 
-public class FileBackedTaskManager implements InMemoryTaskManager  {
+import java.io.File;
 
-}
+public class FileBackedTaskManager extends InMemoryTaskManager { // наследование с возможностью сохранения данных в файл
+    private final File file;
+
+    public FileBackedTaskManager(File file) {
+        this.file = file;
+    }
+
+    //переопределим методы
+    @Override
+
+
