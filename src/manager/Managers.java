eaacbb6f9a7c648134f4e58be2a608fc267a86d5
Lisa <bobronhaim@yandex.ru>
2025-10-06 09:37:39 +0500

    устранение ошибок

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,11 +1,5 @@
 package manager;
 
-import tasks.Epic;
-import tasks.Subtask;
-import tasks.Task;
-
-import java.util.List;
-
 public class Managers {
 
     public static TaskManager getDefault() {
